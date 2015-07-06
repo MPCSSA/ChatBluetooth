ec31917fb9fd70523da098e32695cf15b7bd082c
stg <stefano.giancristofaro.94@gmail.com>
2015-07-06 19:28:23 +0200

    Update mechanism alpha

--- app/src/main/java/com/example/christian/chatbluetooth/view/Activities/ChatActivity.java
+++ app/src/main/java/com/example/christian/chatbluetooth/view/Activities/ChatActivity.java
@@ -77,7 +77,7 @@ public class ChatActivity extends Activity implements ListFragment.OnFragmentInt
 
                     System.out.println(dvc.getAddress() + " found");
 
-                    if (!BlueCtrl.closeDvc.contains(dvc) && /*!dvc.getAddress().equals("1C:B7:2C:0C:60:C6") */!dvc.getAddress().equals("14:DD:A9:3B:53:E3")) {
+                    if (!BlueCtrl.closeDvc.contains(dvc) && !dvc.getAddress().equals("1C:B7:2C:0C:60:C6") /*!dvc.getAddress().equals("14:DD:A9:3B:53:E3")*/) {
                         System.out.println("greetings");
                         BlueCtrl.greet(dvc);
                     }
